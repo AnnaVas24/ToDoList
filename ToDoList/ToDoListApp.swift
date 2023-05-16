3619aa32eca8bd1c30f026aef22e9229edf1516e
AnnaVas24 <95534449+AnnaVas24@users.noreply.github.com>
2023-05-16 15:32:20 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Vasichko Anna on 16.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
